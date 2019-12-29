ee9e9fde8d04b261d409ebba4995d4853b10dda2
0x333333 <jesusjzp@gmail.com>
2019-12-28 22:43:39 -0500

    Fix image rotation.

---
+++ exif.py
@@ -0,0 +1,33 @@
+from PIL import Image, ExifTags
+import os
+
+arr = os.listdir('.')
+
+for filepath in arr:
+    print ">> file: " + filepath
+    if 'jpg' in filepath or 'jpeg' in filepath or 'png' in filepath:
+        try:
+            image=Image.open(filepath)
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation]=='Orientation':
+                    break
+            exif=dict(image._getexif().items())
+
+            if exif[orientation] == 3:
+                image=image.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                image=image.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                image=image.rotate(90, expand=True)
+            else:
+                print "skip"
+                continue
+
+            print "rotate"
+            image.save(filepath)
+            image.close()
+
+        except (AttributeError, KeyError, IndexError, IOError):
+            # cases: image don't have getexif
+            print "failed"
+            pass
