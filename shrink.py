a2b1eaa85efb1f2a9fa578023218094e25ed6399
0x333333 <jesusjzp@gmail.com>
2019-12-31 08:58:54 -0800

    Shrink image size.

---
+++ shrink.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import os
+
+arr = os.listdir('.')
+BASE_WIDTH = 1024
+
+for filepath in arr:
+    print ">> file: " + filepath
+    if 'jpg' in filepath or 'jpeg' in filepath or 'png' in filepath:
+        try:
+            image=Image.open(filepath)
+            width = float(image.size[0])
+            height = float(image.size[1])
+            if width > BASE_WIDTH: 
+                ratio = BASE_WIDTH / width
+                print "ratio: " + str(ratio)
+                width = BASE_WIDTH
+                height = int(height * ratio)
+            else:
+                print "skip"
+
+            image = image.resize((width, height), Image.ANTIALIAS)
+            image.save(filepath)
+            image.close()
+
+        except (AttributeError, KeyError, IndexError, IOError):
+            # cases: image don't have getexif
+            print "failed"
+            pass
